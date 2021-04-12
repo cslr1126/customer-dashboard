6483cef0f927987a6f3a909174fb97953c0eb37c
Cliff Sanchez <clifford.sanchez@liferay.com>
2021-04-12 00:15:33 -0400

    added sticky side nav

--- src/components/MainPanel.js
+++ src/components/MainPanel.js
@@ -1,33 +1,53 @@
 import React from 'react';
 
+const styles = {
+  main: {
+    display: "flex"
+  },
+  card: {
+    padding: "10px",
+    margin: "10px"
+  }
+}
+
 const MainPanel = () => {
     return(
-        <div className="container-fluid">
-            <div className="row">
+        <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
               <div className="header">
                 <p>
                   Welcome back. Here's your account at a glance.
                   You have an upcoming payment of $500
                 </p>
               </div>
-              <div className="col-sm-6">
-                <div className="card">
+              <div className="container" style={styles.main}>
+                <div className="col-sm-4">
+                <div className="card" style={styles.card}>
                   <div className="card-body">
-                    <h5 className="card-title">Gross Sales</h5>
-                    <p className="card-text">$24,323.54</p>
+                    <h5 className="card-title">12</h5>
+                    <p className="card-text">Actions Required</p>
                   </div>
                 </div>
               </div>
-              <div className="col-sm-6">
-                <div className="card">
+              <div className="col-sm-4">
+                <div className="card" style={styles.card}>
+                  <div className="card-body">
+                    <h5 className="card-title">12</h5>
+                    <p className="card-text">Actions Required</p>
+                  </div>
+                </div>
+              </div>
+                <div className="col-sm-4">
+                <div className="card" style={styles.card}>
                   <div className="card-body">
                     <h5 className="card-title">Transactions</h5>
                     <p className="card-text">519</p>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
+              </div>
+             
+          
+          </main>
     )
 }
 
--- src/layouts/HomeLayout.js
+++ src/layouts/HomeLayout.js
@@ -1,22 +1,13 @@
 import React from "react";
-import LeftSideName from "../components/LeftSideNav";
+import StickNav from "../components/StickyNav";
 import MainPanel from "../components/MainPanel";
-import RightSideNav from "../components/RightSideNav";
 
 const HomeLayout = () => {
   return (
     <div className="wrapper container-fluid">
       <div className="row">
-        <div className="col-md-12 col-lg-4 order-lg-1">
-            <LeftSideName />
-        </div>
-        <div className="col-md-12 col-lg-4 order-lg-2ß order-md-2">
-            <MainPanel />
-        </div>
-        <div className="col-md-12 col-lg-4 order-lg-2">
-            <RightSideNav />
-          
-        </div>
+        <StickNav/>
+        <MainPanel />
       </div>
     </div>
   );
