b29b4697a5d457efce38ace1827da2cce1ceaf6f
Cliff Sanchez <clifford.sanchez@liferay.com>
2021-04-12 10:00:14 -0400

    Adjusted sticky bar and margin

--- src/layouts/HomeLayout.js
+++ src/layouts/HomeLayout.js
@@ -2,9 +2,10 @@ import React from "react";
 import StickNav from "../components/StickyNav";
 import MainPanel from "../components/MainPanel";
 
+
 const HomeLayout = () => {
   return (
-    <div className="wrapper container-fluid">
+    <div className="wrapper">
       <div className="row">
         <StickNav/>
         <MainPanel />
